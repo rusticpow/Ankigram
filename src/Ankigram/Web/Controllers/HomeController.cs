458bb197969ca1b7374255c7bb1d9cf092a812dd
Rishat Zakirov <rzakirovt@gmail.com>
2016-04-18 07:37:31 +0300

    Init, WebHook test

---
+++ src/Ankigram/Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using Telegram.Bot.Types;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+
+
+            return View();
+        }
+    }
+}
---
+++ src/Ankigram/Web/Controllers/WebHookController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Telegram.Bot.Types;
+
+
+namespace Web.Controllers
+{
+    public class WebHookController : ApiController
+    {
+        public async Task<IHttpActionResult> Post(Update update)
+        {
+            var message = update.Message;
+
+            Debug.WriteLine("Received Message from {0}", message.Chat.Id);
+
+            if (message.Type == MessageType.TextMessage)
+            {
+                // Echo each Message
+                await Bot.Api.SendTextMessage(message.Chat.Id, message.Text);
+            }
+            else if (message.Type == MessageType.PhotoMessage)
+            {
+                // Download Photo
+                var file = await Bot.Api.GetFile(message.Photo.LastOrDefault().FileId);
+
+                var filename = file.FileId + "." + file.FilePath.Split('.').Last();
+
+                using (var saveImageStream = System.IO.File.Open(filename, FileMode.Create))
+                {
+                    await file.FileStream.CopyToAsync(saveImageStream);
+                }
+
+                await Bot.Api.SendTextMessage(message.Chat.Id, "Thx for the Pics");
+            }
+
+          return Ok();
+        }
+    }
+}
